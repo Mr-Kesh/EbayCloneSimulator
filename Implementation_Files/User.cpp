210c56cf89c5745ed479256618a39397feedbedb
Mr-Kesh <jejedimawea@gmail.com>
2025-03-21 11:41:46 -0600

    f

--- Implementation_Files/User.cpp
+++ Implementation_Files/User.cpp
@@ -5,25 +5,25 @@ User::User(const std::string &username, long phoneNumber, const std::string &add
     : username_(username), phone_no_(phoneNumber), address_(address), account_balance_(accountBalance) {}
 
 // Setter implementations
-void User::setUsername(std::string new_username)
-{
-    username_ = new_username;
-}
+// void User::setUsername(std::string new_username)
+// {
+//     username_ = new_username;
+// }
 
-void User::setPhoneNumber(long new_phone_no)
-{
-    phone_no_ = new_phone_no;
-}
+// void User::setPhoneNumber(long new_phone_no)
+// {
+//     phone_no_ = new_phone_no;
+// }
 
-void User::setAddress(std::string new_address)
-{
-    address_ = new_address;
-}
+// void User::setAddress(std::string new_address)
+// {
+//     address_ = new_address;
+// }
 
-void User::updateBalance(double amount)
-{
-    account_balance_ += amount;
-}
+// void User::updateBalance(double amount)
+// {
+//     account_balance_ += amount;
+// }
 
 void User::updateUserInformation()
 {
