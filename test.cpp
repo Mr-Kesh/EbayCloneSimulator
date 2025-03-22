b80ff9d441d700d9cde35675a221f62399e2c354
Jenna Jones <jenna.jones-2@colorado.edu>
2025-03-22 04:04:58 +0000

    added test file back

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+ #include "catch.hpp"
+ #include "Users.cpp"
+ #include "Buyers.cpp"
+ #include "Sellers.cpp"
+ #include <vector>
+ 
+TEST_CASE( "Temp test case", "[tempTest]" ) {
+    REQUIRE( 1 == 1 );
+}
