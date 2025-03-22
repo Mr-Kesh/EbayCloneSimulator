f0855ed1d35147dda7e3cd8a320cca8bbc38af9a
Mr-Kesh <jejedimawea@gmail.com>
2025-03-21 22:23:26 -0600

    Work

--- test.cpp
+++ test.cpp
@@ -1,8 +1,4 @@
 #define CATCH_CONFIG_MAIN
- #include "catch.hpp"
- #include "User.cpp"
- #include "Buyer.cpp"
- #include "Seller.cpp"
  #include <vector>
  
 TEST_CASE( "Temp test case", "[tempTest]" ) {
